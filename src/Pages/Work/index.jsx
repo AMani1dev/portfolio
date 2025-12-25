4154882141b678ea3038c27a2fa5934b7d03a25a
AMani1dev <139342694+amine1111111@users.noreply.github.com>
2025-12-25 10:48:06 +0100

    uncomment

--- src/Pages/Work/index.jsx
+++ src/Pages/Work/index.jsx
@@ -1,67 +1,3 @@
-// import { Title } from "../../SharedComponents";
-// import { Link, useLoaderData } from "react-router-dom";
-
-// const Cards = ({ projectsData }) => {
-// const base = import.meta.env.BASE_URL;
-
-//   return projectsData.map((project, i) => {
-//     return (
-//       <Link
-//         to={`${project.id}`}
-//         className="max-w-[350px] aspect-square page-transition-link inline-block p-2"
-//         key={i}
-//       >
-//         <div className=" text-gray-900 text-xl">
-//           <img
-//             // src={project?.card?.imgSrc}
-//             src={`${base}${project?.card?.imgSrc}`}
-//             alt="card img"
-//             className="w-full aspect-square "
-//           />
-
-//           <div className="">
-//             <span className="block  font-medium capitalize mt-3 mb-1">
-//               {project?.title}
-//             </span>
-
-//             <hr className="my-3 border-gray-300" />
-//               <span>{project?.card?.yearCreated}</span>
-//           </div>
-//         </div>
-//       </Link>
-//     );
-//   });
-// };
-
-// const Work = () => {
-//   // const data = useLoaderData();
-//   // console.log(data)
-//   // const projectsData = data.projects; 
-//   // console.log(projectsData)
-
-//   const {projects} = useLoaderData();
-
-//   return (
-//     <>
-//       <div className="mt-40">
-//         <Title
-//           title="creat<b>i</b>ng next level <br /> web<b>s</b>ites"
-//           additionClasses=""
-//         />
-//       </div>
-
-//       <section className="flex justify-center flex-wrap gap-10 my-20 p-3">
-//         {/* <Cards projectsData={projectsData} /> */}
-//         <Cards projectsData={projects} />
-//       </section>
-//     </>
-//   );
-// };
-
-// export default Work;
-
-
-
 
 
 import { Title } from "../../SharedComponents";
@@ -122,3 +58,5 @@ const Work = () => {
 };
 
 export default Work;
+
+
